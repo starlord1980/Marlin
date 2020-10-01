af1b623015de68a1b574f200ff2ead303ae6bed8
starlord1980 <jfanthorpe@mac.com>
2020-10-01 21:59:40 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -496,9 +496,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  23.16
+    #define DEFAULT_Ki  2.04
+    #define DEFAULT_Kd  65.62
   #endif
 #endif // PIDTEMP
 
