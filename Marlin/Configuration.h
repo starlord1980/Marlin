b310eccea85419bf8eaf93306be2696dacb76dcf
starlord1980 <jfanthorpe@mac.com>
2020-11-14 21:14:49 +0000

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -990,7 +990,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38.2, 0, -1.73 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -10.8, -1.73 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1378,8 +1378,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS -3
-//#define MANUAL_Y_HOME_POS -4
+#define MANUAL_X_HOME_POS -6
+#define MANUAL_Y_HOME_POS -18.40
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
