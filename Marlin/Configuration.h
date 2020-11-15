dfa9f8c114b7404d180da110ee6f599acaec6276
starlord1980 <jfanthorpe@mac.com>
2020-11-15 20:47:29 +0000

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1379,7 +1379,7 @@
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
 //#define MANUAL_X_HOME_POS -6
-#define MANUAL_Y_HOME_POS -19
+#define MANUAL_Y_HOME_POS -21
 #define MANUAL_Z_HOME_POS 2
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
